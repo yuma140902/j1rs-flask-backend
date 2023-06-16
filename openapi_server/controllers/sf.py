95298e2d68659804a05281637fd59b2400fff59a
yuma14 <yuma140902@gmail.com>
2023-06-16 22:37:38 +0900

    x -> V

--- openapi_server/controllers/sf.py
+++ openapi_server/controllers/sf.py
@@ -1,12 +1,12 @@
 def str_to_func(code_str):
     code = compile(code_str, '<string>', 'eval')
 
-    def f(x, params):
-        return eval(code, {"p": params, "x": x})
+    def f(v, params):
+        return eval(code, {"p": params, "V": v})
     return f
 
 
 if __name__ == '__main__':
-    code = "x * p[0] + p[1]"
+    code = "v * p[0] + p[1]"
     f = str_to_func(code)
     print(f(3, [2, 1]))
